97f7c826f4c1cf044d7c50754163093a553b292a
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-06-13 12:02:43 +0200

    user: Fixed User()

--- user.py
+++ user.py
@@ -3,28 +3,24 @@ import os
 import threading as th
 import json
 
-USER_PATH = "./user.json"
+USER_PATH = """user.json"""
+USER_PATH = os.path.join(os.path.dirname(__file__), USER_PATH)
 
 
 class User:
     '''With Methods that Returns the User's Data'''
-    
-    def __init__(self):
+    def __init__(self, include_repo= False):
         if not os.path.exists(USER_PATH):
             SignUp()
         else:
             with open(USER_PATH) as data:
                 self.Data = json.load(data)
-        
-    def username(self):
-        return str(self.Data['Username'])
-    
-    def password(self):
-        return str(self.Data['Password'])
-
+                self.username = self.Data['Username']
+                self.password = self.Data['Password']
+                if include_repo == True:
+                    self.repo = self.Data['Repos']
 
 
 class SignUp:
     # Will be initilaized later
     '''Sign Up to the App'''
-    pass
