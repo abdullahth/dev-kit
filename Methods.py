dc7141c5b5054a9e7b08295c5ce5fa12b9e98174
abdullahthurwatx@gmail.com <abdullahthurwatx@gmail.com>
2020-06-16 04:55:04 +0200

    browser(), wait()

--- Methods.py
+++ Methods.py
@@ -31,4 +31,33 @@ def export_vars(username, path):
             "export USERNAME=\"{}\"\nexport DIR=\"{}\"".format(username, path)
         )
 
-export_vars('abdullahth', 'e:/Projects')
+
+def browser():
+    '''
+    - Creating Selenium browser and return it
+    - Create WebDriverWait Object and return it
+    return (browser, wait)
+
+    Unpaking:
+    browser, wait = Methods.browser() 
+    Hint: Replace the undesirable element with _
+    '''
+    # Importing Statments
+    from selenium import webdriver  
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.support.ui import WebDriverWait
+
+    # Browser
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    # Wait Object
+    wait = WebDriverWait(browser, timeout=10)
+
+    return(browser, wait)
+
+def wait(driver, timeout):
+    '''
+    Creating Special Wait Object
+    '''
+    from selenium.webdriver.support.ui import WebDriverWait
+    wait = WebDriverWait(driver, timeout)
+    return wait
